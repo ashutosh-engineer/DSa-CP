79453e66c2dcc140411872b67dd97987bfcf52b6
ashutosh-engineer <ashutoshsingh6376@gmail.com>
2026-03-19 23:18:29 +0530

    Easy one

---
+++ Daytona-cost.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        
+        bool found = false;
+        
+        for(int i = 0; i < n; i++){
+            if(a[i] == k){
+                found = true;
+                break;
+            }
+        }
+        
+        if(found) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    
+    return 0;
+}
