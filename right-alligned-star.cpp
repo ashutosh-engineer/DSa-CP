c853e66dc5d732657d4440bff2595ea9d07da51c
ashutosh-engineer <ashutoshsingh6376@gmail.com>
2026-03-21 17:28:00 +0530

    Tricky one

---
+++ right-alligned-star.cpp
@@ -0,0 +1,16 @@
+void staircase(int n) {
+    for(int i = 1; i <= n; i++){
+        
+        // spaces
+        for(int j = 1; j <= n - i; j++){
+            cout << " ";
+        }
+        
+        // stars (#)
+        for(int j = 1; j <= i; j++){
+            cout << "#";
+        }
+        
+        cout << endl;
+    }
+}
