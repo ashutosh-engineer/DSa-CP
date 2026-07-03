018a63a260d69e756adc238804d77d43bfde8167
ashutosh-engineer <ashutoshsingh6376@gmail.com>
2026-07-03 21:24:24 +0530

    Time taken 15 minutes 26 seconds on medium question after seeing pattern on question day-3 of dsa

--- Array/Sliding windows/leetcode1343.cpp
+++ Array/Sliding windows/leetcode1343.cpp
@@ -1,1 +1,32 @@
 //1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+//Time taken 15 minutes 26 seconds;
+
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int n=arr.size();
+        int counter=0;
+        int sum=0;
+        int average=0;
+        for(int i=0; i<k; i++){
+            sum+=arr[i];
+        }
+
+        average = sum / k;
+        if(average >= threshold){
+                counter++;
+        }
+
+        for(int i=k; i<n; i++){
+            sum+=arr[i];
+            sum-=arr[i-k]; //leaving last element
+            average=sum/k;
+
+            if(average >=  threshold){
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+};
