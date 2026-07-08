5989e5b9f063732f32aaef900f4ca87677dd5900
ashutosh-engineer <ashutoshsingh6376@gmail.com>
2026-07-08 12:35:57 +0530

    This was totlay done by claudes help so tommorow dru run this as well as then we will continue to solve all robelsm then will begin with new one from day after tommmorow

--- Array/Sliding windows/sliding_window_maximum.cpp
+++ Array/Sliding windows/sliding_window_maximum.cpp
@@ -1,4 +1,40 @@
 //239. Sliding window maximum
-//Time Complexity:-Big O()
-//Time taken:-
+//Time Complexity:-Big O()n
+//Time taken:-claudes help
+
+lass Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> ans;        // Final result will be stored here
+        deque<int> dq;          // Stores indices of useful elements
+
+                for (int i = 0; i < n; i++) {
+                    
+                    // Step 1: Remove all smaller elements (not useful for max)
+                    while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                        dq.pop_back();
+                    }
+                    
+                    // Step 2: Add current index to deque
+                    dq.push_back(i);
+                    
+                    // Step 3: Remove indices that are out of the current window
+                    if (dq.front() <= i - k) {
+                        dq.pop_front();
+                    }
+                    
+                    // Step 4: Record the maximum for the current window
+                    if (i >= k - 1) {
+                        ans.push_back(nums[dq.front()]);
+                    }
+                }
+
+                return ans;
+
+        
+    }
+};
+
+
 
