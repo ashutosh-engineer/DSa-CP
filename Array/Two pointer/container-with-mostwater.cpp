163d13f31f320eba1546c21e04b854e42e06731e
ashutosh-engineer <ashutoshsingh6376@gmail.com>
2026-07-15 20:10:48 +0530

    SOlved in halh hours

---
+++ Array/Two pointer/container-with-mostwater.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+        int left=0, right=n-1, maxarea=0;
+
+        while(left < right){
+            int area= (right-left) * min(height[left] , height[right]);
+
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+            maxarea=max(maxarea ,area);
+        }
+        return maxarea;
+    }
+};
