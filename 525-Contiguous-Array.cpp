3f083c58f3f85bc0ed988236df31515ed1fdbbdf
Ashutosh singh <163310518+ashutosh-engineer@users.noreply.github.com>
2026-08-04 19:58:10 +0530

    [525] [Time Beats: 80.02%] [Memory Beats: 20.27%] - LeetPush

---
+++ 525-Contiguous-Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        int currsum=0;
+        unordered_map<int , int>mp;
+        mp[0]=-1;
+
+        for(int i=0; i<n; i++){
+            currsum+=(nums[i]==1) ? 1: -1;
+
+            if(mp.find(currsum) != mp.end()){
+                ans=max(ans, i-mp[currsum]);
+            }else{
+                mp[currsum]=i;
+            }
+        }
+
+        return ans;
+        
+    }
+};
