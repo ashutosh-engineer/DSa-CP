3c343f486aca89efe94d75660f527af87ef7db2b
Ashutosh singh <163310518+ashutosh-engineer@users.noreply.github.com>
2026-08-10 20:35:29 +0530

    [1510] [Time Beats: 81.44%] [Memory Beats: 82.55%] - LeetPush

---
+++ DCP-08-26/1510-Stone-Game-IV.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool>dp(n+1, false);
+        if(n==1){
+            return true;
+        }
+
+        for(int i=1; i<=n; ++i){
+            for(int k=1; k*k <=i; ++k){
+                if(!dp[i-k*k]){
+                    dp[i]=true; //Winning state found
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+        
+    }
+};
