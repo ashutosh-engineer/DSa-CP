6c9532a32ce24089d7e47fbc15d091cdd9e58a7e
Ashutosh singh <163310518+ashutosh-engineer@users.noreply.github.com>
2026-08-11 00:28:33 +0530

    [523] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- 523-Continuous-Subarray-Sum.cpp
+++ 523-Continuous-Subarray-Sum.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n=nums.size();
+
         int sum=0;
-        unordered_map<int ,int>mp;
-        mp[0] = -1;
+        unordered_map<int , int>mp;
+
+        if(k % 2 != 0){
+            return false;
+        }
 
         for(int i=0; i<n; i++){
             sum+=nums[i];
 
-            int remainder=(k==0 ? sum : sum % k);
-            if(mp.count(remainder)){
-                if(i - mp[remainder] >= 2) return true;
-            } else {
-                mp[remainder] = i;
+            if(mp[sum] % k==0 && i >= 2){
+                return true;
+            }else{
+                mp[sum]=i;
             }
         }
-
-        return  false;
-
+        return false;
     }
 };
