3ba264720d3691619da54f5dee291673708e28d2
Ashutosh singh <163310518+ashutosh-engineer@users.noreply.github.com>
2026-08-22 11:16:39 +0530

    [3622] [Time Beats: 100.00%] [Memory Beats: 97.17%] - LeetPush

---
+++ DCP-08-26/3622-Check-Divisibility-by-Digit-Sum-and-Product.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int digitsum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    int product(int n) {
+        int prod = 1;
+        while (n > 0) {
+            prod *= n % 10;
+            n /= 10;
+        }
+        return prod;
+    }
+
+    bool checkDivisibility(int n) {
+        int total = digitsum(n) + product(n);
+        return (n % total == 0);
+    }
+};
